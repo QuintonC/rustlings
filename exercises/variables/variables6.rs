40d4e2ec430e84b8f29837a98c09eee9ac05eeaf
Quinton Chester <quinton.chester@shopify.com>
2023-02-07 10:03:44 -0600

    Beginning quiz sections

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,11 @@
 // variables6.rs
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+const NUMBER: i32 = 3;
 fn main() {
+    // let test = 3i32;
+    // note to self, constants must be typed on usage
+    // lets can be scoped and strictly bound on declaration.
+
     println!("Number {}", NUMBER);
 }
